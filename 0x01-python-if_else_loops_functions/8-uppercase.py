41755f1b2da0678e9c2e73cec7141de5cf2fb1ce
Emmanuel Oyibo <emminexworld@gmail.com>
2022-12-07 11:37:16 +0000

    Added some new fixes

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strings = " "
+    for char in str:
+        if (ord(char) >= 97) and (ord(char) <= 122):
+            strings +=(chr(ord(char) - 32))
+        else:
+            strings += char
+    print(strings)
