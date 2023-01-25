f9bb4e59915795be5af2c4643cd3f6914eb365a7
Emmanuel Oyibo <emminexworld@gmail.com>
2023-01-25 22:48:13 +0000

    First commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width):
+        self.width = width
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def __init__(self, height):
+        self.height = height
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return ((self.width * 2) + (self.height * 2))
