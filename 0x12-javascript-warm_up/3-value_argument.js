33ca641333965ff99a1e465d937abfd02365c41a
Emmanuel Oyibo <emminexworld@gmail.com>
2023-03-14 21:40:54 +0000

    first commit

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log('No arguments');
+} else {
+  console.log(args[2]);
+}
