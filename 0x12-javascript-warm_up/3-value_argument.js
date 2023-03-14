590c8e070bec0b13b90d19a1d2d836ecae59acd8
Emmanuel Oyibo <emminexworld@gmail.com>
2023-03-14 21:43:20 +0000

    quick fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const process = require('process');
 const args = process.argv;
 
 if (args[2] === undefined) {
-  console.log('No arguments');
+  console.log('No argument');
 } else {
   console.log(args[2]);
 }
