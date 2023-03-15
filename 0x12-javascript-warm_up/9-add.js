e48ca63ae75e3eaf7b34656a9a5d07472d014e88
Emmanuel Oyibo <emminexworld@gmail.com>
2023-03-15 14:34:12 +0000

    first commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+
+function add (a, b) {
+  return (a + b);
+}
+
+const num1 = parseInt(args[2]);
+const num2 = parseInt(args[3]);
+
+console.log(add(num1, num2));
