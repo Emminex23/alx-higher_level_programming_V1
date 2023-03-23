2c45435c11f723b1e227213189b67c3764b8ef85
Emmanuel Oyibo <emminexworld@gmail.com>
2023-03-23 12:29:26 +0000

    quick fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,7 @@ class Square extends require('./5-square') {
       for (let i = 0; i < this.height; i++) {
         let row = '';
         for (let j = 0; j < this.width; j++) {
-          row += 'c';
+          row += c;
         }
         console.log(row);
       }
