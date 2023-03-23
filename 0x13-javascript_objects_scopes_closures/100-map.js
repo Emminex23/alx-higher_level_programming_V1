2814a5cfdb691b3743f97bdc9545edbf59d3f66e
Emmanuel Oyibo <emminexworld@gmail.com>
2023-03-23 16:03:39 +0000

    fixed a bug

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
-const newList = list.map(num => num * list.indexOf(num));
+const newList = list.map((num, index) => num * index);
 console.log(list);
 console.log(newList);
