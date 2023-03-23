dd0f39a65c5a21b0e5bc1dc33d009aa5a1a43ad5
Emmanuel Oyibo <emminexworld@gmail.com>
2023-03-23 10:48:37 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = h;
+      this.height = w;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+module.exports = Rectangle;
+module.exports = Square;
